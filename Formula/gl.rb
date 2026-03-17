caf39e39e54ec47af3134f751e05348154b6195f
Kevin Codex <kevin@gitlawb.com>
2026-03-17 12:36:34 +0800

    feat: initial Homebrew formula for gl v0.1.2

---
+++ Formula/gl.rb
@@ -0,0 +1,35 @@
+class Gl < Formula
+  desc "Decentralized Git CLI with DID-based identity and IPFS storage"
+  homepage "https://gitlawb.com"
+  license "MIT"
+  version "0.1.2"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/gitlawb/releases/releases/download/v#{version}/gitlawb-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "369e79ba224e78a0d6b6abb653309401dbeaf972f10dc27f0aca8a8ada7b6918"
+    else
+      url "https://github.com/gitlawb/releases/releases/download/v#{version}/gitlawb-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "51bafafebb6157b1e4d1cc33441c271764c9bf98d1d0ed3472411647daaf40a5"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/gitlawb/releases/releases/download/v#{version}/gitlawb-v#{version}-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "c776d73661e68e154efc4607291e6491332dd66ecfafe76d9b7e88ff7e4e2326"
+    else
+      url "https://github.com/gitlawb/releases/releases/download/v#{version}/gitlawb-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "1fba7e9b2b37d0e24bd81675c63660e0f62eb35e9ca1596ed1006263eab6c94e"
+    end
+  end
+
+  def install
+    bin.install "gl"
+    bin.install "git-remote-gitlawb"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gl --version")
+  end
+end
